8772ed33c50cd7ef593da67c8b2cdf81e2f557b9
Pandarem <pandarem.thauront@yahoo.com>
2018-02-20 16:47:33 +0100

    Decomentarized
    I just de-coment some part of this file

--- src/FenetreBillard.java
+++ src/FenetreBillard.java
@@ -1,30 +1,18 @@
-import java.awt.*; //layouts, color, ActionEvent e, ...
-import javax.swing.*; //widgets (JButton, JPanel, ...)
+import java.awt.*;
+import javax.swing.*;
 
 public class FenetreBillard extends JFrame {
 	
 	private Panel mainPanel;
 	
 	public FenetreBillard () {
-		super ("fenetre de dessin");
+		super ("Gentleman");
 		
-		this.setSize(new Dimension(1200, 600)); //fenetre avec les dimension fourni par l'utilisateur
+		this.setSize(new Dimension(1200, 600)); 
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); //procedure normal de fermeture
         
-        /*
-        blablabla 
-        */
 		mainPanel = new Panel ();
 		this.setContentPane (mainPanel); //conteneur principal dans la fenetre
 		this.setVisible(true);
 	}
 }
-
-/*
-JButton
-JFrame
-JLabel
-JPanel
-JTextField
-JTextArea
-*/
