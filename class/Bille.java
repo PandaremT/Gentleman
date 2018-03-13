d7ade05908c25c8ba70fd3aa074b4d3fd32c495c
TotoLocoo <37110609+TotoLocoo@users.noreply.github.com>
2018-03-13 15:06:27 +0100

    Bille.java

--- class/Bille.java
+++ class/Bille.java
@@ -1,12 +1,14 @@
 public class Bille {
 
+
+		int Niter=10000;
 		private int CordoneeX;
 		private int CordoneeY;
 		private int Rayon;
-		private double[][] Vitesse=[2][Niter];
-		private double[][] Acceleration=[2][Niter];
-		private double[][] Position= [2][Niter];
-		private double[][] ForceImpact= [2][Niter];
+		private double[][] Vitesse= new double [2][Niter];
+		private double[][] Acceleration=new double [2][Niter];
+		private double[][] Position= new double [2][Niter];
+		private double[][] ForceImpact= new double[2][Niter];
 		private double Masse;
 		private double Delta_t=0.01;
 		private double FrottementRoule;
@@ -15,6 +17,7 @@ public class Bille {
 			Yinitial=CordoneeY;
 			Xinitial= CordoneeX;
 			Rayon= 0.0615;
+
 			Vitesse[1][0]= 0.0;
 			Vitesse[2][0]= 0.0;
 			Acceleration[1][0]=0.0;
@@ -47,7 +50,7 @@ public class Bille {
 										Position [j][i+1]= Position[j][i] + Vitesse[j][i+1]*Delta_t;
 
 									}
-									if Vitesse[1][i]=0 %% Vitesse[2][i]=0{
+									if (Vitesse[1][i]==0 %% Vitesse[2][i]==0){
 										break;
 									}
 							}
