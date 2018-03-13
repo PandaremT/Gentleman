c8e53a22b6b356a0cfefffcafb3aa45ae1ccdd7f
Pandarem <pandarem.thauront@yahoo.com>
2018-03-13 15:02:43 +0100

    azrt

--- src/Joueur.java
+++ src/Joueur.java
@@ -1,13 +1,17 @@
 public class Joueur {
 	
+	static final int ROUGE = 0;
+	static final int JAUNE = 1;
 	private String nickname;
 	private boolean isWin;
+	private int couleur;
 	
-	public Joueur (String nickname) {
-		
+	public Joueur (String nickname, int couleur) {
 		this.nickname = nickname;
+		this.couleur = couleur;
 		
 	}
+	
 	
 	
 	
