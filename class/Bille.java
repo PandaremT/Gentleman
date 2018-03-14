032648ed383fb5cce4a83f8010211d01ac1438e8
TotoLocoo <37110609+TotoLocoo@users.noreply.github.com>
2018-03-14 19:17:28 +0100

    Bille
    Se compile bien... :8ball:

--- class/Bille.java
+++ class/Bille.java
@@ -1,10 +1,11 @@
+import java.lang.*;
 public class Bille {
 
 
 		int Niter=10000;
 		private int CordoneeX;
 		private int CordoneeY;
-		private int Rayon;
+		private double Rayon;
 		private double[][] Vitesse= new double [2][Niter];
 		private double[][] Acceleration=new double [2][Niter];
 		private double[][] Position= new double [2][Niter];
@@ -23,37 +24,32 @@ public class Bille {
 			Acceleration[1][0]=0.0;
 			Acceleration[2][0]=0.0;
 			Position[1][0]= Xinitial;
-			Prisition[2][0]= Yinitial;
+			Position[2][0]= Yinitial;
 			ForceImpact[1][0]=0.0;
 			ForceImpact[2][0]=0.0;
 			Masse= 0.209;
 		}
 
-		public int getX(){
-			return CordoneeX;
+		public double getX(){
+			return Position[1][0];
 		}
 
-		public int getY(){
-			return CordoneeY;
+		public double getY(){
+			return Position[2][0];
 		}
 
-		public int [][] getPosition(){
-			return Possition;
-		}
 
-		public int [][] colisionBille(double[][] Force){
-			ForceImpact = Force;
+		public void colisionBille(double[] Force){
+
 							for(int i=0; i<Niter ; i++){
-									for(int j=1;i=2;i++){
-										Acceleration[j][i+1]= (ForceImpact-FrottementRoule)/Masse;
+									for(int j=1; j<=2 ;j++){
+										Acceleration[j][i+1]= ((Force[j])/(Masse));
 										Vitesse[j][i+1]= Vitesse[j][i] + Acceleration[j][i+1]*Delta_t;
 										Position [j][i+1]= Position[j][i] + Vitesse[j][i+1]*Delta_t;
 
 									}
-									if (Vitesse[1][i]==0 %% Vitesse[2][i]==0){
-										break;
-									}
+
 							}
-		return Position[][];
+
 		}
 }
