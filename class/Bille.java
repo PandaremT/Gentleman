a1ff70f5c10eb85b2af297da5b292be473dec783
TotoLocoo <37110609+TotoLocoo@users.noreply.github.com>
2018-03-14 19:50:43 +0100

    Update
    Fin de la fonction déplacement

--- class/Bille.java
+++ class/Bille.java
@@ -40,16 +40,40 @@ public class Bille {
 
 
 		public void colisionBille(double[] Force){
+			double PxProvisoire=0;
+			double PyProvisoire=0;
 
+			//Calcule en fonction de la force d'impact (au temps t) l'acceleration, la vitesse, et la position au temps t+1;
+			//Les vitesses et positions sont stockées dans des matrices (2xNiter)
 							for(int i=0; i<Niter ; i++){
 									for(int j=1; j<=2 ;j++){
 										Acceleration[j][i+1]= ((Force[j])/(Masse));
 										Vitesse[j][i+1]= Vitesse[j][i] + Acceleration[j][i+1]*Delta_t;
 										Position [j][i+1]= Position[j][i] + Vitesse[j][i+1]*Delta_t;
 
-									}
+										//Ici se fait le déplacement de la bille;
+										//Le code pour gerer les interfaces graphiques
+										// des billes doit etre placé ici
 
+										if (Vitesse [1][i]==0 && Vitesse[2][i]==0) {
+											PxProvisoire= Position[1][i];
+											PyProvisoire= Position[2][i];
+											break;
+										}
+									}
+									if (Vitesse [1][i]==0 && Vitesse[2][i]==0) break;
 							}
+			//Réinitialisation des matrices temporelles, en gardant la derniere position dans Position[:][0]
+							for (int i=0; i<Niter; i++){
+									for(int j=1;j<=2;j++){
+									Acceleration[j][i]=0;
+									Vitesse[j][i]=0;
+									Position[j][i]=0;
+									}
+							}
+
+							Position[1][0]= PxProvisoire;
+							Position[2][0]= PyProvisoire;
 
 		}
 }
