bac86f743c10580ce1bc7d342d80ff656292cd98
TotoLocoo <37110609+TotoLocoo@users.noreply.github.com>
2018-03-20 09:51:09 +0100

    Comment
    Faut mettre les nores des forces sous forme de coodonnées en x et y vers  le centre de la bille

--- class/Tir.java
+++ class/Tir.java
@@ -1,33 +1,35 @@
 public class Tir {
-	
+
 	final private int MODE_RADIAN = 0;
 	final private int MODE_DEGREE = 1;
-	
+
 	private double [] force;
-	
+
+	//Il faut que la force soti donnée par direction, ligne 1= force en x, ligne 2 = force en y 
+
 	public Tir () {
 		force = new double [2];
 		force[0] = 0;
 		force[1] = 0;
 	}
-	
+
 	public Tir (double angle, double norme) {
 		force = new double [2];
 		force[0] = 0;
 		force[1] = 0;
-		
+
 		this.setForce(angle, norme);
 	}
-	
+
 	public void setForce (double[] force){
 		this.force=force;
 	}
-	
+
 	public void setForce (double angle, double norme) {
 		force [0] = norme*Math.cos(angle);
 		force [1] = norme*Math.sin(angle);
 	}
-	
+
 	public double[] getForce () {
 		return force;
 	}
